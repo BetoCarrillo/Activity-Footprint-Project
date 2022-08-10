b896514d7a43b29e4f4ff6e3326ecb88bef2dd5b
BetoCarrillo <alberto.carrillo01@gmail.com>
2022-08-10 19:57:04 +0200

    Commit with sidebar

--- main2.js
+++ main2.js
@@ -1,11 +1,3 @@
-// Local data results
-
-//console.log(response)
-//console.log(response2)
-
-const divAccordion = document.getElementById("accordionPanelsStayOpenExample");
-
-
 ////// popover bootstrap
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
@@ -23,7 +15,6 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-
 /* function fetchData() {
     fetch(url, requestOptions)
         .then((response) => {
@@ -33,33 +24,26 @@ var requestOptions = {
 
 } */
 
-
 const fetchData = async () => {
 try {  fetch(url, requestOptions)
         .then((response) => {
             return response.json();
         }).then((result) => {
-            myController(result); 
-            let liveData = result.results;
-            createAcc(liveData);
-            createDropdown(liveData);
-        })
-     
-    
-} catch (error) {
+            LD = result.results
+            myController(result.results)
+        })}catch (error) {
     console.log(error)
+   
 }
 
 }
 
-fetchData()
 
 ////////////// ACCORDION
+const divAccordion = document.getElementById("accordionFlushExample");
+const createAcc = (data) => {
 
-
-const createAcc = (liveData) => {
-
-    for (let i = 0; i < liveData.length; i++) {
+    for (let i = 0; i < data.length; i++) {
     let divAcItem = document.createElement("div")
     divAcItem.setAttribute("class", "accordion-item container")
     divAcItem.setAttribute("style", "background-color: #e7f1ff")
@@ -67,43 +51,42 @@ const createAcc = (liveData) => {
 
 
     let h2Ac = document.createElement("div")
-    h2Ac.setAttribute("id", "panelsStayOpen-headingOne")
+    h2Ac.setAttribute("id", "flush-headingOne")
     h2Ac.setAttribute("class", "accordion-header ")
  
 
-    let button = document.createElement("div")
-    button.setAttribute("class", "text-start accordion-button  row")
+    let button = document.createElement("button")
+    button.setAttribute("class", "text-start accordion-button collapsed row")
     button.setAttribute("type", "button")
     button.setAttribute("data-bs-toggle", "collapse")
-    button.setAttribute("data-bs-target", "#panelsStayOpen-collapseOne"+i)
+    button.setAttribute("data-bs-target", "#flush-collapseOne"+i)
     button.setAttribute("aria-expanded", "false")
-    button.setAttribute("aria-controls", "panelsStayOpen-collapseOne")
+    button.setAttribute("aria-controls", "flush-collapseOne")
     button.setAttribute("id", "noMargin")
   
 /// AJUSTAR TITULOS PEQUE
     let divName = document.createElement("div")
     divName.setAttribute("class", "col-md-3")
-    divName.innerHTML = liveData[i].name
+    divName.innerHTML = data[i].name
 
     let divCat = document.createElement("div")
     divCat.setAttribute("class", "col-md-3")
-    divCat.innerHTML = liveData[i].category
-    //divCat.classList.add("red")
-    //divCat.setAttribute("style", "color: red")
+    divCat.innerHTML = data[i].category
 
     let divReg = document.createElement("div")
     divReg.setAttribute("class", "col-md-3")
-    divReg.innerHTML = liveData[i].region_name
+    divReg.innerHTML = data[i].region_name
 
     let divSource = document.createElement("div")
     divSource.setAttribute("class", "col-md-3")
-    divSource.innerHTML = liveData[i].source
-     // LINKEAR SOURCE LINK
+    divSource.innerHTML = data[i].source
+////// LINKEAR SOURCE LINK
     
     let divCollpse = document.createElement("div")
-    divCollpse.setAttribute("id", "panelsStayOpen-collapseOne"+i)
+    divCollpse.setAttribute("id", "flush-collapseOne"+i)
     divCollpse.setAttribute("class", "accordion-collapse collapse container text-start")
-    divCollpse.setAttribute("aria-labelledby", "panelsStayOpen-headingOne")
+        divCollpse.setAttribute("aria-labelledby", "flush-headingOne")
+        divCollpse.setAttribute("data-bs-parent", "#accordionFlushExample")
 
     let divAcBody = document.createElement("div")
     divAcBody.setAttribute("class", "accordion-body row")
@@ -112,29 +95,29 @@ const createAcc = (liveData) => {
     divBodyTwo.setAttribute("class", "col-md-4")
 
     let divDesc = document.createElement("div")
-    divDesc.innerHTML = liveData[i].description
+    divDesc.innerHTML = data[i].description
     divDesc.setAttribute("class", "col-md-8")
 
     let divEm = document.createElement("div")
-    divEm.innerHTML = "Emission Factors: " +liveData[i].factor
+    divEm.innerHTML = "Emission Factors: " + data[i].factor
     divEm.setAttribute("class", "col-md-12")
 
     let divUn = document.createElement("div")
-    divUn.innerHTML = "Unit Type: " +liveData[i].unit
+    divUn.innerHTML = "Unit Type: " + data[i].unit
     divUn.setAttribute("class", "col-md-12")
 
     let divSOr = document.createElement("div")
-    divSOr.innerHTML = "Origin: " + liveData[i].factor_calculation_origin
+    divSOr.innerHTML = "Origin: " + data[i].factor_calculation_origin
     divSOr.setAttribute("class", "col-md-12")
    
     let divYear = document.createElement("div")
-    divYear.innerHTML = "Year: " +liveData[i].year
+    divYear.innerHTML = "Year: " + data[i].year
     divYear.setAttribute("class", "col-md-12")
 
     let divCalc = document.createElement("div")
     divCalc.innerHTML =  
         "Calculation Method: "
-    + liveData[i].factor_calculation_method 
+    + data[i].factor_calculation_method 
     divCalc.setAttribute("class", "col-md-12")
 
     divAcBody.appendChild(divDesc)
@@ -159,33 +142,54 @@ const createAcc = (liveData) => {
 }
 
 //////// Dropdown options
+/// NAME
 const createDropdown = (liveData) => {
-    const dropdown = document.getElementById("name");
-    liveData.forEach((name)=> {console.log(liveData.name)})
-
+    const dropdown = document.querySelector("#nameIn");
+    const activities = liveData.map((data) => {return data.name; });
+    const uniqueActivity = [...new Set(activities)];
+    //console.log(uniqueActivity); 
+    uniqueActivity.forEach((uniqueName) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueName;
+    option.value = uniqueName;
+    dropdown.appendChild(option);
+    })
 }; 
 
 
-/////// CONTROLLER 
-
-function myController(result) {
-   
-
-   
-}
+/////// FUNCTION CONTROLLER 
+function myController(LD) {
+    createAcc(LD);
+    createDropdown(LD);
+    setEventlisteners(LD);
+    
+};
 
 //// event listeners
-const setEvent = () => {
-    document.querySelector("#Name").addEventListener("change", (event) => {});
- 
-}
+const setEventlisteners = (data) => {
+    document.querySelector("#nameIn").addEventListener("change", (event) => {
+        //console.log('selectorworking');
+        console.log(event.target.value);
+        let nameValue = event.target.value
+        filterDropdown(data);
+        return nameValue;
+    });
+};
 
 //// filter dropdown
 
-const filterDropdown = () => {
-    const dropDownValue = document.querySelector("#Name").value;
+const filterDropdown = (data) => {
+    //console.log(actName);
+    const dropDownValue = document.querySelector("#nameIn").value; 
+    const filteredName = data.filter((data) => {
+    
+        return data.name === dropDownValue;
+ 
+    })
+    createAcc(filteredName);
 };
 
+fetchData();
 
 
 
