f4f587946dba1a6a128c6a64298f13831471c796
BetoCarrillo <alberto.carrillo01@gmail.com>
2022-08-11 22:41:36 +0200

    Filters working individually

--- main2.js
+++ main2.js
@@ -1,3 +1,18 @@
+/* /// PENDINGS
+.Ajustar flechas accordion
+.Poner link source
+.AJUSTAR bold titulos: pantalla pequeña
+.footer
+.combinar rilters 
+.Acs.desc emmissions
+.Search bar - FINDE
+.Style - MARTES 
+.Pagination?
+.Geolocation?
+
+//// */
+
+
 ////// popover bootstrap
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
@@ -40,9 +55,10 @@ try {  fetch(url, requestOptions)
 
 
 ////////////// ACCORDION
-const divAccordion = document.getElementById("accordionFlushExample");
-const createAcc = (data) => {
 
+const createAcc = (data) => {
+    const divAccordion = document.getElementById("accordionFlushExample");
+   divAccordion.innerHTML=""
     for (let i = 0; i < data.length; i++) {
     let divAcItem = document.createElement("div")
     divAcItem.setAttribute("class", "accordion-item container")
@@ -60,11 +76,10 @@ const createAcc = (data) => {
     button.setAttribute("type", "button")
     button.setAttribute("data-bs-toggle", "collapse")
     button.setAttribute("data-bs-target", "#flush-collapseOne"+i)
-    button.setAttribute("aria-expanded", "false")
+    button.setAttribute("aria-expanded", "true")
     button.setAttribute("aria-controls", "flush-collapseOne")
     button.setAttribute("id", "noMargin")
   
-/// AJUSTAR TITULOS PEQUE
     let divName = document.createElement("div")
     divName.setAttribute("class", "col-md-3")
     divName.innerHTML = data[i].name
@@ -80,7 +95,6 @@ const createAcc = (data) => {
     let divSource = document.createElement("div")
     divSource.setAttribute("class", "col-md-3")
     divSource.innerHTML = data[i].source
-////// LINKEAR SOURCE LINK
     
     let divCollpse = document.createElement("div")
     divCollpse.setAttribute("id", "flush-collapseOne"+i)
@@ -141,11 +155,11 @@ const createAcc = (data) => {
 
 }
 
-//////// Dropdown options
+//////// Dropdown options////////////////////////////////////////////
 /// NAME
 const createDropdown = (liveData) => {
     const dropdown = document.querySelector("#nameIn");
-    const activities = liveData.map((data) => {return data.name; });
+    const activities = liveData.map((data) => { return data.name; });
     const uniqueActivity = [...new Set(activities)];
     //console.log(uniqueActivity); 
     uniqueActivity.forEach((uniqueName) => {
@@ -156,43 +170,346 @@ const createDropdown = (liveData) => {
     })
 }; 
 
+/////CATEGORY
 
-/////// FUNCTION CONTROLLER 
-function myController(LD) {
-    createAcc(LD);
-    createDropdown(LD);
-    setEventlisteners(LD);
+const createDropdownCat = (liveData) => {
+    const dropdown = document.querySelector("#catIn");
+    const categories = liveData.map((data) => {return data.category;});
+    const uniqueCat = [...new Set(categories)];
+    uniqueCat.forEach((uniqueCat) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueCat;
+    option.value = uniqueCat;
+    dropdown.appendChild(option);
+    })
+}; 
+
+
+/////REGION
+const createDropdownReg = (liveData) => {
+    const dropdown = document.querySelector("#regIn");
+    const regions = liveData.map((data) => {return data.region_name;});
+    const uniqueReg = [...new Set(regions)];
+    uniqueReg.forEach((uniqueReg) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueReg;
+    option.value = uniqueReg;
+    dropdown.appendChild(option);
+    })
+}; 
+
+
+/////SOURCE
+const createDropdownSo = (liveData) => {
+    const dropdown = document.querySelector("#soIn");
+    const sources = liveData.map((data) => {return data.source;});
+    const uniqueSo = [...new Set(sources)];
+    uniqueSo.forEach((uniqueSo) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueSo;
+    option.value = uniqueSo;
+    dropdown.appendChild(option);
+    })
+}; 
+
+/////ORIGIN
+
+const createDropdownOr = (liveData) => {
+    const dropdown = document.querySelector("#orIn");
+    const origins = liveData.map((data) => {return data.factor_calculation_origin;});
+    const uniqueOr = [...new Set(origins)];
+    uniqueOr.forEach((uniqueOr) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueOr;
+    option.value = uniqueOr;
+    dropdown.appendChild(option);
+    })
+}; 
+
+/////YEAR
+
+const createDropdownYe = (liveData) => {
+    const dropdown = document.querySelector("#yeIn");
+    const years = liveData.map((data) => {return data.year;});
+    const uniqueYe = [...new Set(years)];
+    uniqueYe.forEach((uniqueYe) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueYe;
+    option.value = uniqueYe;
+    dropdown.appendChild(option);
+    })
+}; 
+
+
+/////CALCULATION METHOD
+
+const createDropdownMet = (liveData) => {
+    const dropdown = document.querySelector("#metIn");
+    const methods = liveData.map((data) => {return data.factor_calculation_method;});
+    const uniqueMet = [...new Set(methods)];
+    uniqueMet.forEach((uniqueMet) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueMet;
+    option.value = uniqueMet;
+    dropdown.appendChild(option);
+    })
+}; 
+
+
+/////UNIT TYPE
+
+const createDropdownUn = (liveData) => {
+    const dropdown = document.querySelector("#unIn");
+    const units = liveData.map((data) => {return data.unit;});
+    const uniqueUn = [...new Set(units)];
+    uniqueUn.forEach((uniqueUn) => {
+    let option = document.createElement("option");
+    option.innerHTML = uniqueUn;
+    option.value = uniqueUn;
+    dropdown.appendChild(option);
+    })
+}; 
+
+
+
+/////// FUNCTION CONTROLLER ////////////////////////////////////////////
+function myController(data) {
+    createAcc(data);
+
+    createDropdown(data);
+    setEventlisteners(data);
+
+    createDropdownReg(data);
+    setEventlistenersReg(data);
+createDropdownCat(data);
+    setEventlistenersCat(data);
+createDropdownSo(data);
+    setEventlistenersSo(data);
+createDropdownOr(data);
+    setEventlistenersOr(data);
+createDropdownYe(data);
+    setEventlistenersYe(data);
+createDropdownMet(data);
+    setEventlistenersMet(data);
+createDropdownUn(data);
+    setEventlistenersUn(data);
     
 };
 
-//// event listeners
+//// event listeners////////////////////////////////////////////
+/// NAME
 const setEventlisteners = (data) => {
     document.querySelector("#nameIn").addEventListener("change", (event) => {
+        let nameValue = ""
         //console.log('selectorworking');
         console.log(event.target.value);
-        let nameValue = event.target.value
-        filterDropdown(data);
-        return nameValue;
+        nameValue = event.target.value
+        filterDropdown(data); 
     });
 };
 
-//// filter dropdown
+/////CATEGORY
 
+const setEventlistenersCat = (data) => {
+    document.querySelector("#catIn").addEventListener("change", (event) => {
+        let catValue = ""
+        console.log(event.target.value);
+        catValue = event.target.value
+        filterDropdownCat(data); 
+    });
+};
+
+////REGION
+
+const setEventlistenersReg = (data) => {
+    document.querySelector("#regIn").addEventListener("change", (event) => {
+        let regValue = ""
+        //console.log('selectorworking');
+        console.log(event.target.value);
+        regValue = event.target.value
+        filterDropdownReg(data); 
+    });
+};
+
+/////SOURCE
+const setEventlistenersSo = (data) => {
+    document.querySelector("#soIn").addEventListener("change", (event) => {
+        let soValue = ""
+        console.log(event.target.value);
+        soValue = event.target.value
+        filterDropdownSo(data); 
+    });
+};
+
+
+/////ORIGIN
+
+const setEventlistenersOr = (data) => {
+    document.querySelector("#orIn").addEventListener("change", (event) => {
+        let orValue = ""
+        console.log(event.target.value);
+        orValue = event.target.value
+        filterDropdownOr(data); 
+    });
+};
+
+
+/////YEAR
+
+const setEventlistenersYe = (data) => {
+    document.querySelector("#yeIn").addEventListener("change", (event) => {
+        let yeValue = ""
+        console.log(event.target.value);
+        yeValue = event.target.value
+        filterDropdownYe(data); 
+    });
+};
+
+
+/////CALCULATION METHOD
+
+const setEventlistenersMet = (data) => {
+    document.querySelector("#metIn").addEventListener("change", (event) => {
+        let metValue = ""
+        console.log(event.target.value);
+        metValue = event.target.value
+        filterDropdownMet(data); 
+    });
+};
+
+
+/////UNIT TYPE
+
+const setEventlistenersUn = (data) => {
+    document.querySelector("#unIn").addEventListener("change", (event) => {
+        let unValue = ""
+        console.log(event.target.value);
+        unValue = event.target.value
+        filterDropdownUn(data); 
+    });
+};
+
+
+//// filter dropdowns////////////////////////////////////////////
+////NAME
 const filterDropdown = (data) => {
     //console.log(actName);
     const dropDownValue = document.querySelector("#nameIn").value; 
     const filteredName = data.filter((data) => {
-    
-        return data.name === dropDownValue;
- 
+        return data.name === dropDownValue;  
     })
-    createAcc(filteredName);
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredName); 
+}
 };
+
+/////CATEGORY
+
+const filterDropdownCat = (data) => {
+    const dropDownValue = document.querySelector("#catIn").value; 
+    const filteredCat = data.filter((data) => {
+        return data.category === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredCat); 
+}
+};
+
+/////// REGION
+
+const filterDropdownReg = (data) => {
+    const dropDownValue = document.querySelector("#regIn").value; 
+    const filteredReg = data.filter((data) => {
+        return data.region_name === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredReg); 
+}
+};
+
+/////SOURCE
+
+const filterDropdownSo = (data) => {
+    const dropDownValue = document.querySelector("#soIn").value; 
+    const filteredSo = data.filter((data) => {
+        return data.source === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredSo); 
+}
+};
+
+
+/////ORIGIN
+
+const filterDropdownOr = (data) => {
+    const dropDownValue = document.querySelector("#orIn").value; 
+    const filteredOr = data.filter((data) => {
+        return data.factor_calculation_origin === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredOr); 
+}
+};
+
+
+/////YEAR
+
+const filterDropdownYe = (data) => {
+    const dropDownValue = document.querySelector("#yeIn").value; 
+    const filteredYe = data.filter((data) => {
+        return data.year === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredYe); 
+}
+};
+
+
+/////CALCULATION METHOD
+
+const filterDropdownMet = (data) => {
+    const dropDownValue = document.querySelector("#metIn").value; 
+    const filteredMet = data.filter((data) => {
+        return data.factor_calculation_method === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredMet); 
+}
+};
+
+
+/////UNIT TYPE
+
+const filterDropdownUn = (data) => {
+    const dropDownValue = document.querySelector("#unIn").value; 
+    const filteredUn = data.filter((data) => {
+        return data.unit === dropDownValue;  
+    })
+
+   if ( dropDownValue === "all") {createAcc(data)
+    createAcc(data);
+} else {createAcc(filteredUn); 
+}
+};
+
 
 fetchData();
 
 
-
-
-
+    
 
